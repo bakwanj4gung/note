478fb12b203d75e4e9f685cfb7423fb315757432
Muhammad Faqih Ali <alimuhammadfaqih@gmail.com>
2026-03-20 14:56:59 +0300

    update: add new color blockquote: red, yellow, green. default: blue

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,44 @@ import Markdown from 'react-markdown'
 import markdown from './pages/EXAMPLE.md';
 import remarkIns from 'remark-ins';
 
+function blockquoteType(children: any) {
+	try {
+		const objectChild = children?.[1];
+		const text = objectChild?.props?.children?.[0];
+		// info & default are same
+		if (text.includes("[INFO]")) return "info";
+		if (text.includes("[IMPORTANT]")) return "important";
+		if (text.includes("[WARNING]")) return "warning";
+		if (text.includes("[OK]")) return "ok";
+		return "default";
+	} catch (e) {
+		return "default";
+	}
+}
+
+function sanitizeBlockquote(children: any) {
+	try {
+		const objectChild = children?.[1];
+		const text = objectChild?.props?.children?.[0];
+		const removeText = /\[IMPORTANT\]|\[INFO\]|\[WARNING\]|\[OK\]/;
+		console.log(objectChild?.props?.children);
+		if (typeof text === "string" && removeText.test(text)) {
+			const newChildren = objectChild.props.children.slice(3);
+			const newChild = {
+				...objectChild,
+				props: {
+				...objectChild.props,
+				children: newChildren,
+				},
+			};
+			return [children[0], newChild];
+		}
+		return children;
+	} catch (e) {
+		return children;
+	}
+}
+
 function App() {
 	return (
 		<div className="size-full min-h-screen px-8 md:px-16 lg:px-32 xl:px-48">
@@ -10,7 +48,16 @@ function App() {
 				<Navbar />
 				<main className="py-4 space-y-8">
 					<div dir="rtl" className="content">
-						<Markdown remarkPlugins={[remarkIns]}>
+						<Markdown 
+							remarkPlugins={[remarkIns]}
+							components={{ blockquote({ children }) {
+								const type = blockquoteType(children);
+								const sanitizedChildren = sanitizeBlockquote(children);
+								return (
+									<blockquote className={`blockquote-${type}`}>{sanitizedChildren}</blockquote>
+								)
+							}}}
+						>
 							{markdown}
 						</Markdown>
 					</div>
